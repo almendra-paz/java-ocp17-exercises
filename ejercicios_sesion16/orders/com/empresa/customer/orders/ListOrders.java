057b8133e6e328f341e45cc1d4471d2495421916
apaz <apazssd@gmail.com>
2023-04-19 19:38:24 -0500

    ejercicios sesion 16

---
+++ ejercicios_sesion16/orders/com/empresa/customer/orders/ListOrders.java
@@ -0,0 +1,11 @@
+package com.empresa.customer.orders;
+
+public class ListOrders{
+	public void imprimir(){
+		System.out.println("Listado de órdenes");
+	}
+	/*
+	public static void main(String args[]){
+		System.out.println("Listado de órdenes");
+	}*/
+}
